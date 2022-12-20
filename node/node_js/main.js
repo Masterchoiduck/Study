95014304a86bb0bf2c281c8b22f3e37a22fa6fae
csh <csh98419@naver.com>
2022-12-20 23:54:05 +0900

    2022_12_20 공부

--- node/node_js/main.js
+++ node/node_js/main.js
@@ -25,6 +25,28 @@ console.log(os.cpus());
 
 // -----------------------------------------------
 
+const fs = require('fs');
+
+// console.log('Start');
+
+// let content = fs.readFileSync('./new', 'utf8');
+// console.log(content);
+
+// console.log('Finish');
+
+console.log('Start');
+
+fs.readFile('./new', 'utf8',(error,data)=>{
+    console.log(data);
+    console.log(error);
+})
+
+console.log('Finish');
+
+//  readFileSync함수 → 동기 실행
+//  readFile함수 → 비동기 실행 → 비동기 프로그래밍(node js를 잘할려면 비동기 실행을 잘해야함)
+
+// ----------------------------------------------------------------
 const cowsay = require('cowsay')
 console.log(cowsay.say({
   text : "I love jvascript"
